937d5f81abca794699b4f82bad697d77d5a1c427
Hao.Hoang <Hao.Hoang@jdpa.com>
2024-09-04 16:19:15 +0700

    setup the deployment

--- src/views/Main.tsx
+++ src/views/Main.tsx
@@ -133,7 +133,7 @@ export default function Main() {
             <a tw="hover:text-blue-500" href="https://creativecommons.org/licenses/by-nc-sa/4.0/">
               CC BY-NC-SA 4.0
             </a>
-            <span tw="ml-2">2016-present © varHarrie</span>
+            <span tw="ml-2">2016-present © VanHaoHoang</span>
           </FooterCenter>
         </Footer>
       </Wrapper>
